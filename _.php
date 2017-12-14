82597dcea98cc76fd03e55ebc0d8ca956a334c4f
id01 <gaviniboom@gmail.com>
2017-12-14 05:00:44 +0000

    modified:   _.php 	modified:   app.yaml

--- _.php
+++ _.php
@@ -1,5 +1,6 @@
 <?php
 // Redirect to index.html
-header('Location: index.html');
+header("Location: index.html");
+// Print redirection message
+echo '<html><head><body>This page has been moved <a href="index.html">here</a>.</body></html>';
 ?>
-<html><body>This page has been moved <a href="index.html">here</a>.</body></html>
